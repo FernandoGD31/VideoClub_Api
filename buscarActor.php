5df8585141a3d28607cf5f503a38156ff9a90973
CRISGA94 <84537997+CRISGA94@users.noreply.github.com>
2021-07-07 17:16:03 -0500

    Update buscarActor.php

--- buscarActor.php
+++ buscarActor.php
@@ -8,7 +8,7 @@ include('navbar.php');
 
  <div class="container txt-color mt-5">
         <h3 class="titulo">Buscar Director</h3><br>
-        <form method="POST" action="mostrarActor.php" class="row g-3">
+        <form method="POST" action="mostrarActorBuscar.php" class="row g-3">
             <div class="col-md-4">
                 <label class="form-label ">Nombre</label>
                 <input type="text" class="form-control" name="nombre">
